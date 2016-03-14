8feaa1b3ec09ddb37737d411a24a311bacdf7b6c
zhaofei0129 <494709671@qq.com>
2016-03-14 23:52:50 +0800

    Initial Commit

---
+++ ZFWeChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZFWeChat
+//
+//  Created by 赵飞 on 16/3/14.
+//  Copyright © 2016年 赵飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
